f3b0373116eb1029ecbee81160da60691bbe0ef5
Terry Fedirko <terry.fedirko@memexOEE.com>
2021-08-24 00:28:36 -0400

    Add Table Schema

---
+++ DatabaseExercise2/dbo/Tables/CustomerInfo.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[CustomerInfo] (
+    [Id]           UNIQUEIDENTIFIER CONSTRAINT [DF_CustomerInfo_Id] DEFAULT (newid()) NOT NULL,
+    [Firstname]    NVARCHAR (50)    NULL,
+    [LastName]     NVARCHAR (50)    NULL,
+    [Address]      NVARCHAR (255)   NULL,
+    [PostalCode]   NVARCHAR (10)    NULL,
+    [Phone]        NVARCHAR (50)    NULL,
+    [City]         NVARCHAR (50)    NULL,
+    [Province]     NVARCHAR (50)    NULL,
+    [Country]      NVARCHAR (50)    NULL,
+    [Email]        VARCHAR (256)    NULL,
+    [MedicationId] UNIQUEIDENTIFIER NULL,
+    [DeleteFlag]   BIT              NULL,
+    CONSTRAINT [PK_CustomerInfo] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ DatabaseExercise2/dbo/Tables/Medications.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Medications] (
+    [Id]         UNIQUEIDENTIFIER CONSTRAINT [DF_Medications_Id] DEFAULT (newid()) NOT NULL,
+    [Medication] VARCHAR (50)     NOT NULL,
+    CONSTRAINT [PK_Medications] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
